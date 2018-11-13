e684287199b90f9ea9ad0f96ca944a2ba3227f79
Dael Saint-Surin <daelss45@gmail.com>
2018-11-13 00:19:14 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,18 +1,18 @@
 # 
-node default {
-  class { 'dev_environment':
-    user { "daelss45":
-	ensure		=> present,
-	manage_home	=> true,
-	group		=> ['wheel'],
-    }
-    package { "vim":
-	ensure		=> present,
-    }
-    file { "/home/daelss45/.vimrc":
-	ensure		=> file,
-	source		=> "puppet:///vagrant/.vimrc",
-    }
-  }
-}	
+ node default {
+   class { 'dev_environment':
+     user { "daelss45":
+       ensure      => present,
+       manage_home => true,
+       group       => ['wheel'],
+     }
+     package { "vim":
+       ensure      => present,
+     }
+     file { "/home/daelss45/.vimrc":
+       ensure      => file,
+       source      => "puppet:///vagrant/.vimrc",
+     }
+   }
+ }
 
